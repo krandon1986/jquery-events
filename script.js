abd4dda29e7b4cc93f7b9d06ea4c22678ad85919
krandon1986 <krandon1986@yahoo.co.uk>
2023-04-05 14:36:49 +0000

    no comments

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+ 	$("#stream1_btn").on(function() {
+ 		$(".stream1").fadeTo(1000, 0.5);
+
+
 	});
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
@@ -19,6 +18,7 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
-
-
+	$("#myButton").removeClass("blueBox").addClass("border");
+	$("p").css("color", "blue").slideUp(2000).slideDown(2000);
+	$("a").attr("href", "http://www.example.com");
 }); 
